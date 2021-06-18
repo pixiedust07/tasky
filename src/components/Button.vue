523bd801bd20cef672e70385f9fc56c517b1d7b5
Sharon Shaji <sharonshaji127001@gmail.com>
2021-06-18 20:31:06 +0530

    Add Header and Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="addTask" class="btn" :style="{background: color}">{{ btnTitle }}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button', 
+    props: {
+        btnTitle: String,
+        color: String,
+    },
+    methods: {
+        addTask() {
+            console.log('Task Added successfully');
+        }
+    }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button btnTitle="Add new task" color="rgb(68 193 91)"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+export default {
+    name: "Header",
+    props: {
+        title: {
+            type: String,
+            default: "Task Lister App",
+        },
+    },
+    components: {
+        Button,
+    }
+
+}
+</script>
+
+<style scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+</style>
